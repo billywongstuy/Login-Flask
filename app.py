ec4a72e375455085fc0d37f707d04282644e1292
billywongstuy <bwong5@stuy.edu>
2016-10-06 13:05:06 -0400

    another commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
-import csv, hashlib
+import csv, hashlib, os
 
 def hash(word):
     myHashObject = hashlib.sha1()
@@ -21,6 +21,14 @@ def authenticate(username,password):
     return "NOT EXIST"
 
 
+def isUserCookieValid():
+    sheet = csv.reader(open("data/users.csv",'r'))
+    for row in sheet:
+        #how do you check if the key exists
+        if session["username"] and row and session["username"] == row[0]:
+            return True
+    return False
+
 def addUser(username,password):
     sheet = open("data/users.csv",'r')
     read = csv.reader(sheet)
@@ -36,9 +44,14 @@ def addUser(username,password):
     write.writerow([username,hash(password)])
     return True
 
+
 @app.route("/")
 def login():
-    return render_template("login.html")
+    #check the cookie
+    if isUserCookieValid() == True:
+        return render_template("home.html")
+    else:
+        return render_template("login.html")
 
 @app.route("/auth",methods=["POST"])
 def check():
@@ -47,18 +60,31 @@ def check():
     #print request.args
     #print request.args['username']
     #print request.headers
-    success = authenticate(request.form['username'],request.form['password'])
     textToPrint = ""
     link = ""
     linkTxt = ""
-    if success == "YES":
-        textToPrint = "Login successful!"
-    elif success == "NOT EXIST":
-        textToPrint = "User does not exist. Register here:"
-        link = "/register"
-        linkTxt = "Register Here"
-    else:
-        textToPrint = "Login failed!" 
+    if request.form["submit"] == "Login":
+        success = authenticate(request.form['username'],request.form['password'])
+        if success == "YES":
+            textToPrint = "Login successful!"
+            app.secret_key = os.urandom(32)
+            session["username"] = request.form['username']
+        elif success == "WRONG":
+            textToPrint = "Login failed!"
+        else:
+            textToPrint = "You broke the page"
+    elif request.form["submit"] == "Register":
+        if addUser(request.form['username'],request.form['password']):
+            textToPrint = "Registration successful!"
+            link = "/"
+            linkTxt = "Click here to return to the login page."
+        else:
+            textToPrint = "Registration failed!"
+            link = "/register"
+            linkTxt = "Click here to return to the registration page."
+    elif request.form["submit"] == "Logout":
+        session.pop("username")
+        return redirect("login.html")
     return render_template("result.html",result=textToPrint,link=link,linkText = linkTxt)
 
 
@@ -67,7 +93,8 @@ def reg():
     return render_template("register.html")
 
 
-@app.route("/newuser",methods=['POST'])
+
+somestring = '''@app.route("/newuser",methods=['POST'])
 def create():
     textToPrint = ""
     link = ""
@@ -80,7 +107,13 @@ def create():
         textToPrint = "Registration failed!"
         link = "/register"
         linkTxt = "Click here to return to the registration page."
-    return render_template('result.html',link=link,result=textToPrint,linkText = linkTxt)
+    return render_template('result.html',link=link,result=textToPrint,linkText = linkTxt)'''
+
+
+@app.route("/jacobo")
+def js():
+    return redirect("http://xkcd.com")
+
 
 if __name__ == "__main__":
     app.debug = True
